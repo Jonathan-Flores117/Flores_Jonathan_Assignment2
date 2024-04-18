895012cb288319a0096d3db2bb847c516e6c53e9
Jonathan-flores117 <jonathanflores3510@gmail.com>
2024-04-18 10:38:26 -1000

    cowboecb

--- Assignment 2/server.js
+++ Assignment 2/server.js
@@ -1,3 +1,20 @@
+//const users_reg_data = require(__dirname + '.user_data.json')
+const fs = require('fs');
+
+const user_data_file = __dirname + '/user_data.json';
+
+//check that user file exists and read in if it does
+let users_reg_data = {};
+if(fs.existsSync(user_data_file)) {
+  const data = fs.readFileSync(user_data_file,'utf-8');
+   users_reg_data = JSON.parse(data);
+ let stats = fs.statSync(user_data_file);
+ console.log(`${user_data_file} has ${stats.size} characters`)
+
+}
+console.log(users_reg_data);
+// ^^^this is the lab code that was copied from ex1.js ^
+
 // loads the products array into server memory from the products.json file
 const products = require(__dirname + '/products.json');
 
@@ -11,6 +28,22 @@ app.all('*', function (request, response, next) {
   console.log(request.method + ' to ' + request.path);
   next();
 });
+
+
+
+app.post("/login", function (request, response) {
+  //accessing the request body
+  const logindata = request.body;
+  //log the proccess the login data here(authenticated user)
+  console.log('login data:',logindata);
+
+  response.json(loginData);
+
+  // Process login form POST and redirect to logged in page if ok, back to login page if not
+
+});
+//same code for login but now for register below
+app.post("/login", function (request, response) 
 
 // A micro-service to return the products data currently in memory on the server as
 // javascript to define the products array
@@ -58,7 +91,7 @@ errors['no_quantities']= 'Must select some games';
 
 });
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
 
 
@@ -84,7 +117,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve login.html when the user accesses the root URL
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/login.html');
 });
 
